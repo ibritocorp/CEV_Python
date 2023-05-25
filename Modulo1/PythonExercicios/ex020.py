41e2bf3ec0d44139c5ef22c9e3cfb36f252cff1e
Ítalo Brito <ibrito.corp@gmail.com>
2023-05-25 20:24:06 -0300

    Resolução do exercício 20

--- Modulo1/PythonExercicios/ex020.py
+++ Modulo1/PythonExercicios/ex020.py
@@ -1,1 +1,16 @@
-# # Leia quatro nomes, sorteie aleatoriamente a ordem de apresentação desses alunos
+# Leia quatro nomes, sorteie aleatoriamente a ordem de apresentação desses alunos
+
+from random import sample, shuffle
+
+nome1 = input('Primeiro aluno: ')
+nome2 = input('Segundo aluno: ')
+nome3 = input('Terceiro aluno: ')
+nome4 = input('Quarto aluno: ')
+
+# Uma solução possível
+lista = [nome1, nome2, nome3, nome4]
+print(lista)
+print(f'A ordem de apresentação será: {shuffle(lista)}')
+
+# Outra solução possível
+print(f'A ordem de apresentação será: {sample([nome1, nome2, nome3, nome4], k=4)}')
