f1a26b61a8fb4cd3e395829bb11d872f69da4a13
Ítalo Brito <ibrito.corp@gmail.com>
2023-05-25 17:23:36 -0300

    Criação de exercícios resolvidos pelo Guanabara

---
+++ Modulo1/aulas/aula08_demo_random.py
@@ -0,0 +1,10 @@
+import random
+import emoji
+num = random.random()
+print(num)
+
+num1 = random.randint(1, 100)
+print(num1)
+
+print(emoji.emojize("Python é TOP :thumbsup:", language='alias'))
+print(emoji.emojize("Olá, Mundo :earth_americas:", language='alias'))
