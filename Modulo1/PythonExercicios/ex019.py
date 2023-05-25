a5d217b300182c1e9f6d65b61921f5e4b9cc2905
Ítalo Brito <ibrito.corp@gmail.com>
2023-05-25 20:23:42 -0300

    Resolução do exercício 19

--- Modulo1/PythonExercicios/ex019.py
+++ Modulo1/PythonExercicios/ex019.py
@@ -1,1 +1,9 @@
 # Leia quatro nomes e sorteie aleatoriamente um desses nomes
+
+from random import choice
+
+nome1 = input('Primeiro aluno: ')
+nome2 = input('Segundo aluno: ')
+nome3 = input('Terceiro aluno: ')
+nome4 = input('Quarto aluno: ')
+print(f'Esse(a) foi o(a) escolhido(a): {choice([nome1, nome2, nome3, nome4])}')
