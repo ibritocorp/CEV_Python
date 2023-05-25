8e3e011120abd249c3dc975494f1886ebd8cf010
Ítalo Brito <ibrito.corp@gmail.com>
2023-05-25 17:51:35 -0300

    Resolução do exercício 16

--- Modulo1/PythonExercicios/ex016.py
+++ Modulo1/PythonExercicios/ex016.py
@@ -1,1 +1,15 @@
-# Leia a 
+# Leia um número real qualquer e imprima na tela a sua parte inteira
+
+# Solução simples sem a necessidade de importar uma biblioteca (módulo) ou função específica
+'''n = float(input('Digite um número real com casas decimais: '))
+print(f'O valor {n} possui a parte inteira {int(n)}.')'''
+
+# Outra possibilidade de resolução simples
+'''n = float(input('Digite um número real com casas decimais: '))
+print(f'O valor {n} possui a parte inteira {n:.0f}.')'''
+
+# Solução utilizando uma função específica de uma biblioteca (módulo)
+from math import trunc
+
+n = float(input('Digite um número: '))
+print(f'O número {n} tem a parte inteira {trunc(n)}.')
