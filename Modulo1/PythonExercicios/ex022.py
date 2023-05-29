0078430dcf041dc9e83177ef7da8935b0f3b1389
Ítalo Brito <ibrito.corp@gmail.com>
2023-05-29 16:32:40 -0300

    Correção do Guanabara

--- Modulo1/PythonExercicios/ex022.py
+++ Modulo1/PythonExercicios/ex022.py
@@ -7,6 +7,9 @@ Crie um programa que leia o nome completo de uma pessoa e mostre:
 * Quantas letras tem o primeiro nome
 """
 
+# Meu código
+
+"""
 nome = input('Digite o nome completo: ')
 print(f'Nome em caixa alta: {nome.upper()}')
 print(f'Nome em caixa baixa: {nome.lower()}')
@@ -14,3 +17,16 @@ nomesemespaco = nome.replace(' ', '')
 print(f'Quantidade de caracteres sem espaços: {len(nomesemespaco)}')
 nomedividido = nome.split()
 print(f'Quantidade de caracteres do primeiro nome: {len(nomedividido[0])}')
+"""
+
+
+# Código corrigido
+
+nome = input('Digite o nome completo: ').strip()
+print(f'Nome em caixa alta: {nome.upper()}')
+print(f'Nome em caixa baixa: {nome.lower()}')
+print(f'Quantidade de caracteres sem espaços: {len(nome) - nome.count(" ")}')
+# print(f'Quantidade de caracteres do primeiro nome: {nome.find(" ")}')
+# Ou utilizar a função 'split'
+separa = nome.split()
+print(f'Seu primeiro nome é {separa[0]} e ele tem {len(separa[0])}')
