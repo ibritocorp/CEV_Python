6f179232cc61bd9f7009f8d1411603910dbdada3
Ítalo Brito <ibrito.corp@gmail.com>
2023-05-31 15:54:33 -0300

    Atualização

--- Modulo1/aulas/aula11_demo3.py
+++ Modulo1/aulas/aula11_demo3.py
@@ -2,6 +2,6 @@
 
 cores = {'limpa':'\033[m',
          'c1':'\033[0;31;40m',
-         'c2':'\033[1;30;43m'}
+         'c2':'\033[1;31;107m'}
 
 print(f'{cores["c1"]}Olá{cores["limpa"]}, {cores["c2"]}Mundo{cores["limpa"]}!')
