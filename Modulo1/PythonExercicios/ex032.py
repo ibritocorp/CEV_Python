0c1f0f12df95b847265b4ad3feaa5955958a2400
Ítalo Brito <ibrito.corp@gmail.com>
2023-05-30 23:00:07 -0300

    Solução do exercício 32

---
+++ Modulo1/PythonExercicios/ex032.py
@@ -0,0 +1,19 @@
+"""
+Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO.
+"""
+
+# Importação da função 'date' da biblioteca 'datetime'
+from datetime import date
+
+# Estrutura de leitura do ano
+ano = int(input('Qual o ano deseja analisar? Digite "0" caso queira analisar o ano atual. '))
+
+# Estrutura condicional simples para verificar se é o ano atual através do valor digitado '0'
+if ano == 0:
+    ano = date.today().year
+
+# Estrutura condicional composta para condição de bissexto
+if ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0):
+    print(f'O ano {ano} é BISSEXTO!')
+else:
+    print(f'O ano {ano} NÃO é BISSEXTO!')
