4174d72a1ad82174ca14a7ad00ce7a3ad43547bc
Ítalo Brito <ibrito.corp@gmail.com>
2023-06-20 20:36:31 -0300

    Aula 13 - Estruturas de repetição

---
+++ Modulo2/aulas/aula13_demo.py
@@ -0,0 +1,15 @@
+# Estrutura para contar de 0 a 5
+for c in range(0, 6): # Contagem no Python vai até o último número do parâmetro -1
+    print(c)
+print('FIM')
+
+
+# Estrutura para contar de 5 a 0 (decrescendo)
+for c in range(5, -1, -1): # Comando -1 ele decresce a contagem
+    print(c)
+print('FIM')
+
+# Estrutura para uma contagem (nesse caso de 0 a 6) pulando de 2 em 2
+for c in range(0, 7, 2): # Comando -1 ele decresce a contagem
+    print(c)
+print('FIM')
