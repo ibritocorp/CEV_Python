b5767c981601a50f635e30f06b3d8e8f81c5736a
Ítalo Brito <ibrito.corp@gmail.com>
2023-06-20 23:25:38 -0300

    Solução do exercício 54

---
+++ Modulo2/exercicios/ex054.py
@@ -0,0 +1,17 @@
+'''
+Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+'''
+
+from datetime import date
+
+contMaiorIdade = 0
+contMenorIdade = 0
+
+for i in range(1, 7):
+    anoNascimento = int(input('Digite o ano de nascimento: ').strip())
+    if date.today().year - anoNascimento >= 21:
+        contMaiorIdade += 1
+    else:
+        contMenorIdade += 1
+
+print(f'{contMaiorIdade} pessoas atingiram a MAIOR IDADE e {contMenorIdade} ainda NÃO ATINGIRAM A MAIOR IDADE!')
