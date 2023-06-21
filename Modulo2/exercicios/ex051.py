22538aea906f0d2dc93b08d779512721da734b41
Ítalo Brito <ibrito.corp@gmail.com>
2023-06-20 22:38:05 -0300

    Solução exercício 51

---
+++ Modulo2/exercicios/ex051.py
@@ -0,0 +1,12 @@
+'''
+Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final mostre os 10 primeiros termos dessa progressão.
+'''
+
+priTermo = int(input('Digite o primeiro termo de uma Progressão Aritmética: ').strip())
+razao = int(input('Digite a razão de uma Progressão Aritmética: ').strip())
+n = priTermo
+
+for cont in range(1, 10):
+    print(f'{n} {"-> "}', end='')
+    n += razao
+print('FIM')
