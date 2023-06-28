3d083c5c994429d500b7995a1750937d6f965e1a
Ítalo Brito <ibrito.corp@gmail.com>
2023-06-28 19:09:22 -0300

    Correções do exercício 61

---
+++ Modulo2/exercicios/ex060_correcao2.py
@@ -0,0 +1,12 @@
+n = c = int(input('Digite um número para calcular seu Fatorial: '))
+f = 1
+
+print(f'Calculando "{n}!": ', end='')
+
+while c > 0:
+    print(f'{c}', end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+
+print(f)
