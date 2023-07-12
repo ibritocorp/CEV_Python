831e4b40fae7545697b3b1a235cce9c59bc0ab92
Ítalo Brito <ibrito.corp@gmail.com>
2023-07-12 17:05:00 -0300

    Update ex067.py

--- Modulo2/exercicios/ex067.py
+++ Modulo2/exercicios/ex067.py
@@ -2,8 +2,6 @@
 Faça um programa que mostre a tabuada de vários números, um de cada vez para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
 '''
 
-num = 0
-
 while True:
     num = int(input('Quer ver a tabuada de qual número? '))
 
